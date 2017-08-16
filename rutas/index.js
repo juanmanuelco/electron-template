2b26f966ab5d522ef34744480fffceb0556a21b0
Cuartohokage <Cuartohokage>
2017-08-15 23:00:48 -0500

    funcion denuevo normal

--- rutas/index.js
+++ rutas/index.js
@@ -10,8 +10,7 @@ function ensureAuthenticated(req, res, next) {
 }
 
 //De esta forma mostramos la página principal de nuestra aplicación    
-//Se quita la autenticacion solo por el momento
-router.get('/',function(req,res){
+router.get('/',ensureAuthenticated,function(req,res){
     //usamos la cadena JSON despues de la coma ('index',{ESTA ES LA CADENA JSON}), con el fin de poder enviar datos desde aquí hasta el cliente
     //Para que esto funcione debemos poner {{saludo}} en el handlebars correspondiente
     res.render('index',{saludo:'Hola a todos'})
