77dc7c59a8418b2dc7c67cf159ce76edd583e5bc
pablo_cesar1995@hotmail.com <pablo_cesar1995@hotmail.com>
2017-08-19 14:03:47 -0500

    Modelo creado (prueba)

--- modelos/ventas.js
+++ modelos/ventas.js
@@ -6,7 +6,11 @@ var bcrypt = require('bcryptjs');
 
 //Se define un esquema de como iran los datos guardados_______________________________________________________________________________________
 var EMAEVENTINV_OBJ  = mongoose.Schema({
-	//
+	CodVen_Vent: Number,
+	Ced_Vent: String,//referencia a otro esquema
+	Fech_Vent: Date,
+	CodPro_Vent: Array,//referencia a otro esquema
+	Desc_Vent:Number // Total a pagar de la venta
 });
 
 //Exporta el esquema para poder ser usado en cada ruta que sea  necesario____________________________________________________________________
